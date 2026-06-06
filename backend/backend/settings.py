f17642d2c756a848a66ba0ba68e74c70475b9ca1
snrdvrs <sean.devaras96@yahoo.com>
2026-06-06 13:32:53 +0800

    settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,38 +106,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-database_url = os.environ.get('DATABASE_URL')
-if database_url:
-    parsed_database_url = urlparse(database_url)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': unquote(parsed_database_url.path.lstrip('/')),
-            'USER': unquote(parsed_database_url.username or ''),
-            'PASSWORD': unquote(parsed_database_url.password or ''),
-            'HOST': parsed_database_url.hostname or '',
-            'PORT': parsed_database_url.port or '',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'fLNkDckgQrrjICfwIUZElqWRKDLBbUxH',
+        'HOST': 'kodama.proxy.rlwy.net',
+        'PORT': '55133',
     }
-elif os.environ.get('DB_NAME'):
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
-            'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT', '5432'),
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
